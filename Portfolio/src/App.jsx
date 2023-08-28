949356279b51f7101d414169d3cd64dc902fb227
Mefiasty <freshminter@gmail.com>
2023-08-28 21:12:51 +0200

    repair rensponsive look

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -8,18 +8,18 @@ import NoPage from "./pages/NoPage";
 
 export default function App() {
   return (
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Omnie />} />
-          <Route path="projekty" element={<Projekty />} />
-          <Route path="CV" element={<CV />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Omnie />} />
+            <Route path="projekty" element={<Projekty />} />
+            <Route path="CV" element={<CV />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- Portfolio/src/pages/layout.jsx
+++ Portfolio/src/pages/layout.jsx
@@ -3,15 +3,15 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
     return (
         <>
-            <div className="lg:mb-10">
-                <h1 className="text-8xl">Portfolio</h1>
+            <div className="lg:mb-10 mb-4">
+                <h1 className="text-8xl mb-2">Portfolio</h1>
                 <center>
                     <img src="zdjecie.png" alt="zdjecie.png" srcset="" className="border-2 rounded-full border-zinc-600" />
                 </center>
             </div>
 
             <div className="menu justify-center ">
-                <div className=" lg:h-10  lg:mb-12 mb-6 bg-zinc-400 w-1/2 ml-60">
+                <div className=" lg:h-10  lg:mb-12 mb-6 bg-zinc-400  ">
                     <nav className=" lg:grid lg:grid-cols-3 grid grid-rows-3  ">
 
                         <Link to="/projekty" className="hover:bg-zinc-900 hover:h-10">Projekty</Link>
