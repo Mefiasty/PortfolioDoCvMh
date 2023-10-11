5cb1b0da1a270ce8ad1e07ee3180cd053c744f57
Mefiasty <freshminter@gmail.com>
2023-10-11 18:41:23 +0200

    adding new project

--- Portfolio/src/pages/Projekty.jsx
+++ Portfolio/src/pages/Projekty.jsx
@@ -1,13 +1,37 @@
 const Projekty = () => {
-    return (
-        <>
-            <h1 className="text-6xl m-6">Projekty</h1>
-
-            <a href="https://restauracja-wsei-projekt.vercel.app" target="_blank" className="text-2xl
-                 hover:text-red-700">Projekt nr.1 <center><img src="public/projekt1.png" alt="" srcset="" className="mt-2 " /></center></a>
-
-        </>
-    )
+  return (
+    <>
+      <h1 className="text-6xl m-6">Projekty</h1>
+      <div className="grid grid-cols-3">
+        <div>
+          <a
+            href="https://restauracja-wsei-projekt.vercel.app"
+            target="_blank"
+            className="text-2xl
+                 hover:text-red-700"
+          >
+            Projekt nr.1
+            <center>
+              <img src="/projekt1.png" alt="" srcset="" className="mt-2" />
+            </center>
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://fanpageditto.vercel.app/"
+            target="_blank"
+            className="text-2xl
+                     hover:text-red-700"
+          >
+            Projekt nr.2
+            <center>
+              <img src="/projekt2.png" alt="" srcset="" className="mt-2" />
+            </center>
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Projekty;
